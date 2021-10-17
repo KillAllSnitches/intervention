1ef0d5f63c71e8141c14523085988ab135ee26c4
KillAllSnitches <86169807+KillAllSnitches@users.noreply.github.com>
2021-10-17 02:40:57 -0700

    added bing & fixed search operators
    Added Bing
    Changed the Name Since I Changed My GitHub Name
    Fixed Utilized Search Operators
    Fixed Console Clear Code

--- intervention.py
+++ intervention.py
@@ -4,9 +4,10 @@ from colorama import init, Fore, Style
 import ctypes
 def logo():
     if os.name == "nt":
-        ctypes.windll.kernel32.SetConsoleTitleW('Intervention | Coded by: vx#1234 | github.com/vx-dev')
+        ctypes.windll.kernel32.SetConsoleTitleW('Intervention | Coded by: Kill All Snitches | github.com/killallsnitches')
         os.system('cls')    
-    os.system('clear')
+    else:
+        os.system('clear')
     print(Fore.CYAN + """
   _____ _   _ _______ ______ _______      ________ _   _ _______ _____ ____  _   _ 
  |_   _| \ | |__   __|  ____|  __ \ \    / /  ____| \ | |__   __|_   _/ __ \| \ | |
@@ -16,19 +17,32 @@ def logo():
  |_____|_| \_|  |_|  |______|_|  \_\  \/   |______|_| \_|  |_|  |_____\____/|_| \_|
 
 """)
-    print(Fore.BLUE + """                       File Sniping Helper by: vx#1234""")
+    print(Fore.WHITE + "                       File Sniping Helper by:" + Fore.RED + " Kill All Snitches")
     print(Style.RESET_ALL)
                                                                                                                                 
+def engine():
+    logo()
+    print("""
+    [1] Bing
+    [2] Google
+    """)
+    x = input('Engine: ')
+    if x == '1':
+        bing()
+    elif x == '2':
+        google()
+    else:
+        engine()
 
-def menu():
+
+
+def google():
     anon = 'anonfile.com+'
     driv = 'drive.google.com+'
     medi = 'mediafire.com+'
     zipp = 'zippyshare.com+'
     past = 'pastebin.com+'
-    inurl = 'inurl:'
     site = 'site:'
-    o = '+OR+'
     logo()
     print("""
     [1] Anonfile
@@ -36,36 +50,88 @@ def menu():
     [3] Mediafire
     [4] Zippyshare
     [5] Pastebin
+    [<] Back
     """)
     mode = input('Service: ')
     if mode == '1':
         logo()        
         key = input('Enter your keyword: ')
-        webbrowser.open('https://google.com/search?q='+inurl+anon+key+o+site+anon+key)
-        menu()
+        webbrowser.open('https://google.com/search?q='+site+anon+key)
+        google()
     elif mode == '2':
         logo()
         key = input('Enter your keyword: ')
-        webbrowser.open('https://google.com/search?q='+inurl+driv+key)
-        menu()
+        webbrowser.open('https://google.com/search?q='+site+driv+key)
+        google()
     elif mode == '3':
         logo()
         key = input('Enter your keyword: ')
-        webbrowser.open('https://google.com/search?q='+inurl+medi+key)
-        menu()
+        webbrowser.open('https://google.com/search?q='+site+medi+key)
+        google()
     elif mode == '4':
         logo()
         key = input('Enter your keyword: ')
-        webbrowser.open('https://google.com/search?q='+inurl+zipp+key)
-        menu()
+        webbrowser.open('https://google.com/search?q='+site+zipp+key)
+        google()
 
     elif mode == '5':
         logo()
-        key = input('Enter your keyword')
-        webbrowser.open('https://google.com/search?q='+inurl+past+key)
-        menu()
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://google.com/search?q='+site+past+key)
+        google()
+    elif mode == '<':
+        engine()
     else:
-        print("Invalid Option!")
-        time.sleep(1000)
-        menu()
-menu()
+        google()
+    
+
+def bing():
+    anon = 'anonfile.com+'
+    driv = 'drive.google.com+'
+    medi = 'mediafire.com+'
+    zipp = 'zippyshare.com+'
+    past = 'pastebin.com+'
+    site = 'site:'
+    logo()
+    print("""
+    [1] Anonfile
+    [2] bing Drive
+    [3] Mediafire
+    [4] Zippyshare
+    [5] Pastebin
+    [<] Back
+    """)
+    mode = input('Service: ')
+    if mode == '1':
+        logo()        
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://bing.com/search?q='+site+anon+key)
+        bing()
+    elif mode == '2':
+        logo()
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://bing.com/search?q='+site+driv+key)
+        bing()
+    elif mode == '3':
+        logo()
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://bing.com/search?q='+site+medi+key)
+        bing()
+    elif mode == '4':
+        logo()
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://bing.com/search?q='+site+zipp+key)
+        bing()
+
+    elif mode == '5':
+        logo()
+        key = input('Enter your keyword: ')
+        webbrowser.open('https://bing.com/search?q='+site+past+key)
+        bing()
+    elif mode == '<':
+        engine()
+    else:
+        bing()
+
+
+engine()
